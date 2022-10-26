bd7323ddae0c76b71746bcd19b82db400ba3628a
Melih Namer <mnamer12@outlook.com>
2022-10-26 13:50:34 +0300

    fixes on auth - .env file moved to root on the server

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,17 +1,18 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
+// const { JWT_SECRET ='4f69cb2460f1788cead4a48228074076321547fc8a29f12da7e7dd8087be6422' } = process.env;
+const UnauthorizedError = require('../errors/UnauthorizedError')
 
-
-const errorAuth = (res) => {
-  res.status(401).send({ message: 'Authorization Required'});
-}
+// const errorAuth = (res) => {
+//   res.status(401).send({ message: 'Authorization Required'});
+// }
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
 if (!authorization || !authorization.startsWith('Bearer ')) {
-  return errorAuth(res);
+  return next(new UnauthorizedError('You need authorization to proceed'));
 }
 
 const token = authorization.replace('Bearer ', '');
@@ -20,7 +21,7 @@ let payload;
 try {
   payload = jwt.verify(token, JWT_SECRET );
 } catch (err) {
-  return errorAuth(res);
+  return next(new UnauthorizedError('You need authorization to proceed'));
 }
 
 req.user = payload;
